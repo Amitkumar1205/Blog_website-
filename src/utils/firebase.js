ad80766a2b948d0153357857c702132772c529f6
Amit kumar <kumaramitdev1205jnvjamui@gmail.com>
2023-11-06 21:37:03 +0530

    Blog web

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE,
-  authDomain: "blog-cddc2.firebaseapp.com",
-  projectId: "blog-cddc2",
-  storageBucket: "blog-cddc2.appspot.com",
-  messagingSenderId: "413696194974",
-  appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
+  apiKey: process.env.FIREBASE ,
+  authDomain: "blog-8cc62.firebaseapp.com",
+  projectId: "blog-8cc62",
+  storageBucket: "blog-8cc62.appspot.com",
+  messagingSenderId: "429003124808",
+  appId: "1:429003124808:web:efd8718f9940faaa23687d",
+  measurementId: "G-6CSG17THSL"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
